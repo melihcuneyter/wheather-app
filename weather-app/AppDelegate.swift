1876c4910f0e17ca15057c68886a87266ad04b53
Melih Cüneyter <melihcuneyter@hotmail.com>
2023-02-06 14:20:52 +0300

    Initial project.

---
+++ weather-app/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  weather-app
+//
+//  Created by Melih Cüneyter on 6.02.2023.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+
+        return true
+    }
+}
+
---
+++ weather-app/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  weather-app
+//
+//  Created by Melih Cüneyter on 6.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
+
