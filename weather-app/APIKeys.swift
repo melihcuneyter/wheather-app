cf46f54bfd1f7318b92c40e3464ee8f171d6963c
Melih Cüneyter <melihcuneyter@hotmail.com>
2023-02-09 01:16:22 +0300

    Update APIKeys.swift

--- weather-app/APIKeys.swift
+++ weather-app/APIKeys.swift
@@ -9,6 +9,6 @@ import Foundation
 
 struct APIKeys {
     static let api = "https://api.openweathermap.org"
-    static let apiKey = "e0f66b1faa1d03c25551b5f812a8b94e"
-    static let googlePlacesKey = "AIzaSyDj2L8wivljGGFvh4bNln65QOvAmXYVC5k"
+    static let apiKey = ""
+    static let googlePlacesKey = ""
 }
